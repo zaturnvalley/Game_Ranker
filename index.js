258938eedaa6adee45966b41c4161a6d0f07c49d
zaturnvalley <zacharycmason@gmail.com>
2017-12-01 17:17:20 -0800

    added bootstrap hamburger

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ var path = require('path');
 var isLoggedIn = require('./middleware/isLoggedIn');
 var gameCtrl = require("./controllers/game");
 var profileCtrl = require("./controllers/profile");
+var igdb = require('igdb-api-node').default;
 
 //Global Variables
 var app = express();
@@ -75,28 +76,43 @@ app.get('/', function(req, res) {
 });
 
 app.get('/results', function(req, res){
-  var qs = {
-    search: req.query.q,
+  // var qs = {
+  //   search: req.query.q,
+  //   fields: 'name',
+  //   limit: 30,
+  //   offset: 0,
+  //   r: 'json'
+  // }
+  var client = igdb(process.env.AUTH);
+
+  client.games({
     fields: 'name',
     limit: 30,
-    offset: 0,
-    r: 'json'
-  }
-  request({ 
-    url: 'https://igdbcom-internet-game-database-v1.p.mashape.com/games/',
-    qs: qs,
-    headers: {
-      'X-Mashape-Authorization': process.env.AUTH
-    }
-  }, function(error, response, body){
+    offset: 0, 
+    search: req.query.q
+  }).then((error, response, body) => {
     var data = JSON.parse(body);
     if(!error && response.statusCode == 200){
       res.render('results', {results: data});
     } else {
-      console.log(data);
-      res.send("API currently down. Currently in process of fixing as of 12/1/2017");
+      res.send('error');
     }
   });
+  // request({ 
+  //   url: 'https://api-2445582011268.apicast.io',
+  //   qs: qs,
+  //   headers: {
+  //     'X-Mashape-Authorization': process.env.AUTH
+  //   }
+  // }, function(error, response, body){
+  //   // var data = JSON.parse(body);
+  //   console.log(body);
+  //   // if(!error && response.statusCode == 200){
+  //   //   res.render('results', {results: data});
+  //   // } else {
+  //   //   res.send("API currently down. Currently in process of fixing as of 12/1/2017");
+  //   // }
+  // });
 });
 
 app.use("/game", gameCtrl);
